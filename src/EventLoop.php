0b91dd1260da64627c458cfff7e0dace552c1f4c
Ricardo Pereira <contato@ricardopedias.com.br>
2022-06-21 11:30:28 -0300

    Adicionado o método estático para obter a localização dos comandos implementados

--- src/EventLoop.php
+++ src/EventLoop.php
@@ -12,6 +12,10 @@ class EventLoop
     {
     }
 
+    public static function commandPath(): string
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . 'Command';
+    }
     public function addSubscriber(string $channel, string $subscriberSignatute): void
     {
         $this->publisher->subscribe($channel, $subscriberSignatute);
